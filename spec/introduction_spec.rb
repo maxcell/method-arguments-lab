cbd531a67b161033045aae970dc7b22d8352899f
Prince Wilson <prince@flatironschool.com>
2019-09-09 22:06:16 +0000

    Automatically backed up by Learn

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -1,6 +1,7 @@
 require "spec_helper"
 
-describe "#introduction" do 
+describe "#introduction" do
+  
   it "takes in an argument of a name and puts out a phrase with " do
     expect(1).to be(1)
   end
